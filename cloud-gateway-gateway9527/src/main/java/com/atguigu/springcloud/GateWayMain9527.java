3430ed448538c5281116c2d97d09f00ecf44d020
hanhaotian <1456023704@qq.com>
2020-07-08 23:58:23 +0800

    服务配置：springClod.GateWay

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * <br/>Author hanhaotian
+ * <br/>Description : 主启动类
+ * <br/>CreateTime 2020/7/8
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
